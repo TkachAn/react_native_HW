d41646aa0de64f09c37508e492869a8d5231fb4a
TkachAn <4ndrey.tkachenko@gmail.com>
2023-02-11 21:02:32 +0200

    Create index.js

---
+++ i18n/index.js
@@ -0,0 +1,35 @@
+import * as Localization from "expo-localization";
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+
+import {en} from "./translations/en";
+import {ua} from "./translations/ua";
+import {ru} from "./translations/ru";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  ua: {
+    translation: ua,
+  },
+  ru: {
+    translation: ru,
+  },
+};
+
+console.log("Localization.locale:", Localization.locale);
+i18n.use(initReactI18next).init({
+  compatibilityJSON: "v3",
+  fallbackLng: "en",
+  lng: Localization.locale, //"ua", //
+  resources,
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
+  },
+});
+
+export default i18n;
