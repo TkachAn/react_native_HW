a7943834e2c9aa92bae4811cdedd521ee07f48fa
TkachAn <4ndrey.tkachenko@gmail.com>
2023-02-11 21:02:56 +0200

    i18n

---
+++ i18n/translations/en.js
@@ -0,0 +1,14 @@
+export const en = {
+  SignUp: "Sign Up",
+  SignIn: "Sign In",
+  Login: "Login",
+  Credentials: "Credentials",
+  Reg: "Register",
+  Email: "E-mail address",
+  Send: "SEND",
+  Pass: "Password",
+  Show: "Show",
+  Hide: "Hide",
+  or_in: "or Log in",
+  or_up: "or Register",
+};
